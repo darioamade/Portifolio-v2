623b364d95d8941ed80bb527e5e15cb33f6df303
darioamade <dario_isd@outlook.com>
2020-05-10 23:34:17 +0100

    Added new files

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+    });
+
+
+/*
+    const logox = document.querySelectorAll('#logox path');
+
+	for(let i = 0; i < logox.length; i++) {
+		console.log(`Letter ${i} is ${logox[i].getTotalLength()}`);
+		
+	}
+		*/
